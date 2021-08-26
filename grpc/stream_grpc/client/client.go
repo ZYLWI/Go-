e34ac4b26c33d6c1469a7f0c874ee5b66569ade7
root <root@192.168.43.198>
2021-08-26 23:11:30 +0800

    feature: grpc-stream
    详细描述：grpc流服务基本样例，服务端
    流，客户端流，双向流。

--- grpc/stream_grpc/client/client.go
+++ grpc/stream_grpc/client/client.go
@@ -11,8 +11,9 @@ import (
 )
 
 // Server-side streaming RPC
+// 服务端流模式
 func serverSideRPC() {
-    conn, err := grpc.Dial("localhost:50053", grpc.WithInsecure())
+    conn, err := grpc.Dial("localhost:50055", grpc.WithInsecure())
     if err != nil {
         panic(err)
     }
@@ -20,21 +21,26 @@ func serverSideRPC() {
     defer fmt.Println("conn close")
     c := pb.NewGreeterClient(conn)
 
-    res, _ := c.GetStream(context.Background(), &pb.StreamRequestData{Data: "client send"})
+    data := &pb.StreamRequestData{
+        Data: "client send one msg",
+    }
+
+    stream, _ := c.GetStream(context.Background(), data)
     for {
-        a, err := res.Recv() // socket send recv
+        data, err := stream.Recv() // socket send recv
         if err == io.EOF {
-            fmt.Println("get stream end")
+            fmt.Println("client recv EOF")
             break
         } else if err != nil {
-            fmt.Println(err)
+            fmt.Println("client recv has failed, %v", err)
             break
         }
-        fmt.Println(a.Data)
+        fmt.Println(data.Data)
     }
 }
 
 // Client-side streaming RPC
+// 客户端流模式
 func clientSideRPC() {
     conn, err := grpc.Dial("localhost:50055", grpc.WithInsecure())
     if err != nil {
@@ -46,23 +52,27 @@ func clientSideRPC() {
 
     stream, err := c.PutStream(context.Background())
     if err != nil {
-        fmt.Println("put stream has error: %v", err)
+        fmt.Println("client put has error: %v", err)
     }
+
     for i := 0; i <= 5; i ++ {
-        if err := stream.Send(&pb.StreamRequestData{
+        data := &pb.StreamRequestData{
             Data: fmt.Sprintf("client send stream %d", i),
-        }); err != nil{
-            fmt.Println("client stream send %v", err)
+        }
+        if err := stream.Send(data); err != nil{
+            fmt.Println("client stream send has error %v", err)
         }
         time.Sleep(time.Second)
     }
+
     reply, err := stream.CloseAndRecv()
     if err != nil {
-        fmt.Println("%v.CloseAndRecv() got error %v, want %v", stream, err, nil)
+        fmt.Println("client recv has error %v", err)
     }
-    fmt.Println("recv server reply: %v", reply)
+    fmt.Println("client recv info ", reply)
 }
 
+// 双向流模式
 func bidirectionRPC() {
     conn, err := grpc.Dial("localhost:50055", grpc.WithInsecure())
     if err != nil {
@@ -70,8 +80,8 @@ func bidirectionRPC() {
     }
     defer conn.Close()
     defer fmt.Println("conn close")
-    c := pb.NewGreeterClient(conn)
 
+    c := pb.NewGreeterClient(conn)
     stream, _ := c.AllStream(context.Background())
 
     var wg sync.WaitGroup
@@ -81,27 +91,28 @@ func bidirectionRPC() {
         for{
             in, err := stream.Recv()
             if err == io.EOF {
-                fmt.Println("server has been close, %v", time.Now())
+                fmt.Println("server has been close", time.Now())
                 return
             }
             if err != nil {
                 fmt.Println("failed to receive err: %v", err)
             }
-            fmt.Println("get message from server: %v", in)
+            fmt.Println("client recv data: ", in)
         }
     }()
 
     for i := 0; i <= 5; i ++ {
-        if err := stream.Send(&pb.StreamRequestData{
-            Data: "client send",
-        }); err != nil {
+        data := &pb.StreamRequestData{
+            Data: fmt.Sprintf("client send %v", i),
+        }
+        if err := stream.Send(data); err != nil {
             fmt.Println("Failed to send a note %v", err)
         }
         time.Sleep(time.Second)
     }
 
-    wg.Wait()
     stream.CloseSend()
+    wg.Wait()
 }
 
 func main(){
--- grpc/stream_grpc/server/server.go
+++ grpc/stream_grpc/server/server.go
@@ -5,6 +5,7 @@ import (
     "fmt"
 	"net"
     "time"
+    "sync"
 	"google.golang.org/grpc"
 	pb "stream_grpc/proto"
 )
@@ -16,59 +17,72 @@ type server struct {
 }
 
 // Server-side streaming RPC
-func (s *server) GetStream(req *pb.StreamRequestData, res pb.Greeter_GetStreamServer) error {
-    i := 0
-    for {
-        i ++
-        _ = res.Send(&pb.StreamResponseData{
+// 服务端流模式
+func (s *server) GetStream(req *pb.StreamRequestData, stream pb.Greeter_GetStreamServer) error {
+    for i := 0; i < 5; i ++ {
+        data := &pb.StreamResponseData{
             Data: fmt.Sprintf("server send %v", i),
-        })
-        time.Sleep(time.Second)
-        if i >= 10 {
-            break
         }
+        if err := stream.Send(data); err != nil {
+            fmt.Println("server send stream failed: %v", err)
+            return err
+        }
+        time.Sleep(time.Second)
     }
 	return nil
 }
 
 // Client-side Steaming RPC
+// 客户端流模式
 func (s *server) PutStream(stream pb.Greeter_PutStreamServer) error {
     for {
         data, err := stream.Recv()
         if err == io.EOF {
             return stream.SendAndClose(&pb.StreamResponseData{
-                Data: "server stream will be close",
+                Data: "server will be close",
             })
         }
         if err != nil {
             fmt.Println(err)
             return err
         }
-        fmt.Println(data.Data)
+        fmt.Println("server recv info: ", data.Data)
     }
 }
 
 // Bidirectional streaming RPC
+// 双向流模式
 func (s *server) AllStream(stream pb.Greeter_AllStreamServer) error {
-    for {
-        in, err := stream.Recv()
-        if err == io.EOF{
-            fmt.Println("client has been end, %v", time.Now())
-            return nil
-        }
-        if err != nil {
-            fmt.Println("server recv has err: %v", err)
-            return err
-        }
+    var wg sync.WaitGroup
+    wg.Add(1)
+    go func() {
+        defer wg.Done()
+        for {
+            in, err := stream.Recv()
+            if err == io.EOF{
+                fmt.Println("client has been end", time.Now())
+                return
+            }
+            if err != nil {
+                fmt.Println("server recv has err: %v", err)
+                return
+            }
 
-        fmt.Println("stream recv info %v", in)
+            fmt.Println("server recv info ", in)
+        }
+    }()
 
-        if err := stream.Send(&pb.StreamResponseData{
-            Data: fmt.Sprintf("server send"),
-        }); err != nil {
+    for i := 0; i < 5; i ++ {
+        data := &pb.StreamResponseData{
+            Data: fmt.Sprintf("server send %v", i),
+        }
+        if err := stream.Send(data); err != nil {
             return err
         }
     }
+
+    wg.Wait()
+    return nil
 }
 
 func main() {
